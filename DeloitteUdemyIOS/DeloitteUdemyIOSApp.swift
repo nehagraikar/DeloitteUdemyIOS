cba01ca403a90b46690e0f980dd5f50cb637d544
nehagraikar <nraikar@deloitte.com>
2021-12-11 16:24:47 +0530

    Initial Commit

---
+++ DeloitteUdemyIOS/DeloitteUdemyIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeloitteUdemyIOSApp.swift
+//  DeloitteUdemyIOS
+//
+//  Created by nraikar on 11/12/21.
+//
+
+import SwiftUI
+
+@main
+struct DeloitteUdemyIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
